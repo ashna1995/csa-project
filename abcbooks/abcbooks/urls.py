14f225012dfa0db66e44943f865f0cb4e858572d
ashnapaularackal <ashnapaularackal@gmail.com>
2024-07-25 13:45:01 -0400

    Initial commit

---
+++ abcbooks/abcbooks/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from books import (views)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.landing_page, name='landing_page'),
+    path('books/', views.index, name='index'),
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<str:isbn>/', views.edit_book, name='edit_book'),
+    path('delete/<str:isbn>/', views.delete_book, name='delete_book'),
+]
